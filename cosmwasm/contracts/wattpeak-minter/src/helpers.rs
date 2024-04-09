fb751f120ce85530c6acee2a584635b04b814f37
alexander-astrand <alexander.astrand@gmail.com>
2024-04-09 15:54:28 +0200

    Updated based on notes

--- cosmwasm/contracts/wattpeak-minter/src/helpers.rs
+++ cosmwasm/contracts/wattpeak-minter/src/helpers.rs
@@ -25,3 +25,85 @@ pub fn create_denom_msg(subdenom: String, full_denom: String, denom: NewDenom) -
         }),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::CosmosMsg;
+    use token_bindings::TokenFactoryMsg;
+
+#[test]
+    fn test_create_denom() {
+        fn serialize_message(msg: &TokenFactoryMsg) -> Result<Vec<u8>, cosmwasm_std::StdError> {
+            let serialized_msg = cosmwasm_std::to_vec(&msg)?;
+            Ok(serialized_msg)
+        }
+        // Create an instance of MsgCreateDenom with example data
+        let subdenom = "wattpeak".to_string();
+        let full_denom = "wattpeak".to_string();
+        let denom = NewDenom {
+                name: "WattPeak".to_string(),
+                description: Some("WattPeak is a token that represents the amount of wattpeak that a solarpanel represents".to_string()),
+                symbol: "WattPeak".to_string(),
+                decimals: 0,
+                initial_balances: None,
+            };
+
+        let msg = create_denom_msg(subdenom, full_denom, denom);
+
+        let serialized_msg = serialize_message(&msg).unwrap();
+
+        // The type_url for the MsgCreateDenom based on its Protobuf definition
+        let type_url = "/osmosis.tokenfactory.v1beta1.MsgCreateDenom";
+
+        let expected_msg: CosmosMsg = CosmosMsg::Stargate {
+            type_url: type_url.into(),
+            value: cosmwasm_std::Binary(serialized_msg),
+        };
+
+        if let CosmosMsg::Stargate { type_url, value } = &expected_msg {
+            assert_eq!(type_url, "/osmosis.tokenfactory.v1beta1.MsgCreateDenom");
+            assert!(
+                !value.0.is_empty(),
+                "The serialized message should not be empty"
+            );
+        } else {
+            panic!("Expected CosmosMsg::Stargate variant");
+        }
+    }
+    #[test]
+    fn test_serialization_cycle() {
+
+        fn serialize_message(msg: &TokenFactoryMsg) -> Result<Vec<u8>, cosmwasm_std::StdError> {
+            let serialized_msg = cosmwasm_std::to_vec(&msg)?;
+            Ok(serialized_msg)
+        }
+        let msg = create_denom_msg(
+            "WattPeak".to_string(),
+            "full_wattpeak".to_string(),
+            NewDenom { 
+                name: "WattPeak".to_string(),
+                description: Some("WattPeak is a token that represents the amount of wattpeak that a solarpanel represents".to_string()),
+                symbol: "WattPeak".to_string(),
+                decimals: 0,
+                initial_balances: None,},
+        );
+
+        let serialized = serialize_message(&msg).unwrap();
+        let deserialized: TokenFactoryMsg = cosmwasm_std::from_slice(&serialized).unwrap();
+
+        // Assertions to check the deserialized content matches the original message
+        match deserialized {
+            TokenFactoryMsg::CreateDenom { subdenom, metadata } => {
+                assert_eq!(subdenom, "WattPeak");
+                assert_eq!(metadata.clone().unwrap().name.unwrap(), "WattPeak".to_string());
+                assert_eq!(metadata.clone().unwrap().description, Some("WattPeak is a token that represents the amount of wattpeak that a solarpanel represents".to_string()));
+                assert_eq!(metadata.clone().unwrap().denom_units[0].denom, "full_wattpeak");
+                assert_eq!(metadata.clone().unwrap().denom_units[0].exponent, 0);
+                assert_eq!(metadata.clone().unwrap().denom_units[1].denom, "WattPeak");
+                assert_eq!(metadata.clone().unwrap().denom_units[1].exponent, 0);
+            }
+            _ => panic!("Unexpected message type"),
+        }
+    }
+}
--- cosmwasm/contracts/wattpeak-minter/src/lib.rs
+++ cosmwasm/contracts/wattpeak-minter/src/lib.rs
@@ -1,15 +1,16 @@
 use crate::error::ContractError;
 use crate::msg::InstantiateMsg;
 use crate::state::{
-    AVAILABLE_WATTPEAK_COUNT, CONFIG, PROJECT_DEALS_COUNT, TOTAL_WATTPEAK_MINTED_COUNT,
+    AVAILABLE_WATTPEAK_COUNT, CONFIG, PROJECT_DEALS_COUNT, TOTAL_WATTPEAK_MINTED_COUNT, DECIMALS, DESCRIPTION, FULL_DENOM, NAME, SUBDENOM, SYMBOL,
 };
 use cosmwasm_std::{entry_point, DepsMut, Env, MessageInfo, Response};
 use helpers::create_denom_msg;
 use msg::NewDenom;
+use token_bindings::TokenFactoryMsg;
 
 pub mod error;
 pub mod execute;
-mod helpers;
+pub mod helpers;
 pub mod msg;
 pub mod query;
 pub mod state;
@@ -20,7 +21,7 @@ pub fn instantiate(
     _env: Env,
     _info: MessageInfo,
     msg: InstantiateMsg,
-) -> Result<Response, ContractError> {
+) -> Result<Response<TokenFactoryMsg>, ContractError> {
     msg.config.validate(deps.as_ref())?;
     CONFIG.save(deps.storage, &msg.config)?;
 
@@ -30,19 +31,17 @@ pub fn instantiate(
 
     TOTAL_WATTPEAK_MINTED_COUNT.save(deps.storage, &0).unwrap();
 
-    let subdenom = "wattpeak".to_string();
-    let full_denom = "wattpeak".to_string();
     let denom = NewDenom {
-        name: "WattPeak".to_string(),
-        description: Some("WattPeak is a token that represents the amount of wattpeak that a solarpanel represents".to_string()),
-        symbol: "WattPeak".to_string(),
-        decimals: 0,
+        name: NAME.to_string(),
+        description: Some(DESCRIPTION.to_string()),
+        symbol: SYMBOL.to_string(),
+        decimals: DECIMALS,
         initial_balances: None,
     };
 
-    create_denom_msg(subdenom, full_denom, denom);
+    let create_denom_msg = create_denom_msg(SUBDENOM.to_string(), FULL_DENOM.to_string(), denom);
 
-    Ok(Response::new())
+    Ok(Response::new().add_message(create_denom_msg))
 }
 
 #[cfg(test)]
@@ -50,8 +49,7 @@ mod tests {
     use super::*;
     use crate::state::Config;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coin, coins, Addr, CosmosMsg, Decimal};
-    use token_bindings::TokenFactoryMsg;
+    use cosmwasm_std::{coin, coins, Addr, Decimal};
 
     #[test]
     fn test_initialization() {
@@ -75,8 +73,8 @@ mod tests {
             },
         )
         .unwrap();
-        assert_eq!(0, res.messages.len());
-
+        assert_eq!(1, res.messages.len());
+        
         let loaded_config = CONFIG.load(deps.as_ref().storage).unwrap();
         assert_eq!(loaded_config, config);
 
@@ -94,80 +92,5 @@ mod tests {
             .load(deps.as_ref().storage)
             .unwrap();
         assert_eq!(total_wattpeak_minted, 0);
-    }
-
-    #[test]
-    fn test_create_denom() {
-        fn serialize_message(msg: &TokenFactoryMsg) -> Result<Vec<u8>, cosmwasm_std::StdError> {
-            let serialized_msg = cosmwasm_std::to_vec(&msg)?;
-            Ok(serialized_msg)
-        }
-        // Create an instance of MsgCreateDenom with example data
-        let subdenom = "wattpeak".to_string();
-        let full_denom = "wattpeak".to_string();
-        let denom = NewDenom {
-                name: "WattPeak".to_string(),
-                description: Some("WattPeak is a token that represents the amount of wattpeak that a solarpanel represents".to_string()),
-                symbol: "WattPeak".to_string(),
-                decimals: 0,
-                initial_balances: None,
-            };
-
-        let msg = create_denom_msg(subdenom, full_denom, denom);
-
-        let serialized_msg = serialize_message(&msg).unwrap();
-
-        // The type_url for the MsgCreateDenom based on its Protobuf definition
-        let type_url = "/osmosis.tokenfactory.v1beta1.MsgCreateDenom";
-
-        let expected_msg: CosmosMsg = CosmosMsg::Stargate {
-            type_url: type_url.into(),
-            value: cosmwasm_std::Binary(serialized_msg),
-        };
-
-        if let CosmosMsg::Stargate { type_url, value } = &expected_msg {
-            assert_eq!(type_url, "/osmosis.tokenfactory.v1beta1.MsgCreateDenom");
-            assert!(
-                !value.0.is_empty(),
-                "The serialized message should not be empty"
-            );
-        } else {
-            panic!("Expected CosmosMsg::Stargate variant");
-        }
-    }
-    #[test]
-    fn test_serialization_cycle() {
-
-        fn serialize_message(msg: &TokenFactoryMsg) -> Result<Vec<u8>, cosmwasm_std::StdError> {
-            let serialized_msg = cosmwasm_std::to_vec(&msg)?;
-            Ok(serialized_msg)
-        }
-        let msg = create_denom_msg(
-            "WattPeak".to_string(),
-            "full_wattpeak".to_string(),
-            NewDenom { 
-                name: "WattPeak".to_string(),
-                description: Some("WattPeak is a token that represents the amount of wattpeak that a solarpanel represents".to_string()),
-                symbol: "WattPeak".to_string(),
-                decimals: 0,
-                initial_balances: None,},
-        );
-
-        let serialized = serialize_message(&msg).unwrap();
-        let deserialized: TokenFactoryMsg = cosmwasm_std::from_slice(&serialized).unwrap();
-
-        // Assertions to check the deserialized content matches the original message
-        match deserialized {
-            TokenFactoryMsg::CreateDenom { subdenom, metadata } => {
-                assert_eq!(subdenom, "WattPeak");
-                assert_eq!(metadata.clone().unwrap().name.unwrap(), "WattPeak".to_string());
-                assert_eq!(metadata.clone().unwrap().description, Some("WattPeak is a token that represents the amount of wattpeak that a solarpanel represents".to_string()));
-                assert_eq!(metadata.clone().unwrap().denom_units[0].denom, "full_wattpeak");
-                assert_eq!(metadata.clone().unwrap().denom_units[0].exponent, 0);
-                assert_eq!(metadata.clone().unwrap().denom_units[1].denom, "WattPeak");
-                assert_eq!(metadata.clone().unwrap().denom_units[1].exponent, 0);
-            }
-            _ => panic!("Unexpected message type"),
-        }
     }
 }
