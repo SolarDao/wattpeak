74ae38a63ba0331736c4b690a6e8731a7cac27eb
alexander-astrand <alexander.astrand@gmail.com>
2024-04-16 14:28:38 +0200

    Basic scaffold and instantion of staking contract

---
+++ cosmwasm/contracts/watpeak-staker/src/lib.rs
@@ -0,0 +1,35 @@
+pub mod error;
+pub mod msg; // Add this line to import the `msg` module
+
+use crate::msg::InstantiateMsg;
+use cosmwasm_std::{entry_point, DepsMut, Env, MessageInfo, Response};
+use error::ContractError;
+
+#[entry_point]
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+
+    Ok(Response::new())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+
+    #[test]
+    fn proper_initialization() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {};
+        let info = mock_info("creator", &[]);
+
+        // we can just call .unwrap() to assert this was a success
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(0, res.messages.len());
+    }
+}
