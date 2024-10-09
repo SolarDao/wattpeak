650e66ec8754463db35f5ee870be207adf819c15
alexander-astrand <alexander.astrand@gmail.com>
2024-10-09 03:19:01 +0200

    moved api keys to backend and fetch it from there

--- frontend/components/common/helpers/GoogleMapComponent.tsx
+++ frontend/components/common/helpers/GoogleMapComponent.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { Loading } from "./Loading";
 
-const GoogleMapComponent = (projects: { projects: any[]; }) => {
+const GoogleMapComponent = ({ projects }: { projects: any[] }) => {
+  const [apiKey, setApiKey] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true); // State to manage loading
+
   const mapStyles = {
     height: "400px",
     width: "97%",
@@ -15,26 +19,43 @@ const GoogleMapComponent = (projects: { projects: any[]; }) => {
     lat: 30.7128,
     lng: 13.006,
   };
-  
+
+  // Fetch the API key from the backend
+  useEffect(() => {
+    const fetchApiKey = async () => {
+      try {
+        const res = await fetch("/api/getGoogleMapsApiKey");
+        const data = await res.json();
+        setApiKey(data.apiKey);
+      } catch (error) {
+        console.error("Error fetching API key:", error);
+      } finally {
+        setLoading(false); // Set loading to false once API key is fetched
+      }
+    };
+    fetchApiKey();
+  }, []);
+
+  // Display a loading state until the API key is fetched
+  if (loading || !apiKey) {
+    return <Loading />
+  }
+
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
-    >
+    <LoadScript googleMapsApiKey={apiKey}>
       <GoogleMap mapContainerStyle={mapStyles} zoom={2} center={defaultCenter}>
-        {projects.projects.map((location, index) => {
-          return (
-            <Marker
-              key={index}
-              position={{
-                lat: Number(location.location.latitude),
-                lng: Number(location.location.longitude),
-              }}
-              onClick={() => {
-                // Your click handler
-              }}
-            />
-          );
-        })}
+        {projects.map((location, index) => (
+          <Marker
+            key={index}
+            position={{
+              lat: Number(location.location.latitude),
+              lng: Number(location.location.longitude),
+            }}
+            onClick={() => {
+              // Your click handler
+            }}
+          />
+        ))}
       </GoogleMap>
     </LoadScript>
   );
